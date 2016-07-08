f188299bff124a50a71ac5417a0b5f17d09b0d44
epoluodi <12163687@qq.com>
2016-07-08 17:35:20 +0800

    未完成

--- app/src/main/java/com/pxpd/App/Common.java
+++ app/src/main/java/com/pxpd/App/Common.java
@@ -1,5 +1,12 @@
 package com.pxpd.App;
 
+import android.content.res.XmlResourceParser;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.io.InputStream;
+
 /**
  * Created by Stereo on 16/7/8.
  */
@@ -8,6 +15,38 @@ public class Common {
 
     public static String getjsonForXML(String xml)
     {
+        InputStream inputStream=
+        XmlPullParser parser = Xml.newPullParser();
+        parser.setInput(,"UTF-8");
+        }
+        try {
+            // 直到文档的结尾处
+            while (xrp.getEventType() != XmlResourceParser.END_DOCUMENT) {
+                // 如果遇到了开始标签
+                if (xrp.getEventType() == XmlResourceParser.START_TAG) {
+                    String tagName = xrp.getName();// 获取标签的名字
+                    if (tagName.equals("item")) {
+                        Map<String, String> map = new HashMap<String, String>();
+                        String id = xrp.getAttributeValue(null, "id");// 通过属性名来获取属性值
+                        map.put("id", id);
+                        String url = xrp.getAttributeValue(1);// 通过属性索引来获取属性值
+                        map.put("url", url);
+                        map.put("name", xrp.nextText());
+                        list.add(map);
+                    }
+                }
+                xrp.next();// 获取解析下一个事件
+            }
+        } catch (XmlPullParserException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
         return xml.substring(35,xml.length()-9);
     }
+\
 }
