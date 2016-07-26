f97cc6d0775b9f72c868aa5b81b37e9288c53713
撒旦 <12163687@qq.com>
2016-07-27 00:36:05 +0800

    盘点加载数据

--- app/src/main/java/com/pxpd/App/DB.java
+++ app/src/main/java/com/pxpd/App/DB.java
@@ -14,16 +14,14 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DB {
 
 
-
-    public String userdbpath="";
+    public String userdbpath = "";
     SQLiteDatabase db;
     private Context context;
     SQLiteOpenHelper sqLiteOpenHelper;
 
 
-
     public DB(Context context, String userDbpath) {
-        sqLiteOpenHelper = new SQLiteOpenHelper(context,userDbpath,null,1) {
+        sqLiteOpenHelper = new SQLiteOpenHelper(context, userDbpath, null, 1) {
             @Override
             public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
@@ -35,16 +33,16 @@ public class DB {
             }
         };
 
-        userdbpath= userDbpath;
+        userdbpath = userDbpath;
         //因为getWritableDatabase内部调用了mContext.openOrCreateDatabase(mName, 0, mFactory);
         //所以要确保context已初始化,我们可以把实例化DBManager的步骤放在Activity的onCreate里
         this.context = context;
 
-        db  = sqLiteOpenHelper.getWritableDatabase();
+        db = sqLiteOpenHelper.getWritableDatabase();
     }
 
     public void closeDB() {
-        if (db !=null)
+        if (db != null)
             db.close();
     }
 
@@ -54,5 +52,18 @@ public class DB {
     }
 
 
+    /**
+     * 获取档案数据信息
+     * @return
+     */
+    public Cursor getArchives() {
+        try {
+            Cursor cursor = db.rawQuery("select * from ArchivesManage", null);
+            return cursor;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
--- app/src/main/java/com/pxpd/online_pd.java
+++ app/src/main/java/com/pxpd/online_pd.java
@@ -1,13 +1,18 @@
 package com.pxpd;
 
 import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -17,16 +22,22 @@ import com.android.hdhe.uhf.reader.UhfReader;
 import com.pxpd.App.DB;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import UHF.InventoryThread;
 import cn.pda.scan.ScanThread;
 import cn.pda.serialport.Util;
 
+//201409000000000000020991
+//E2001026750401840890BE8B
+//E2001026750401850890BE8F
+
 
 public class online_pd extends Activity {
     private ImageView btnreturn, btnscan;
-
-    private ScanThread scanThread;
     private TextView weizhi,kufang,mijijia,lie,
             yingyou,zaiku,jieyue,scaned;
     private TextView title;
@@ -34,8 +45,10 @@ public class online_pd extends Activity {
     private DB db;
     private UhfReader reader ;
     private Boolean isstartrfid=false;
-    private int scanmode=0;
     private InventoryThread inventoryThread;
+    private List<Map<String,String>> mapList;
+    private Map<String,Integer>mapqueue;
+    private MyAdpter myAdpter;
 
 
     @Override
@@ -67,12 +80,62 @@ public class online_pd extends Activity {
         zaiku.setText(bundle.getString("OnShelf"));
         jieyue.setText(bundle.getString("Borrow"));
 
+        mapList = new ArrayList<Map<String, String>>();
+        mapqueue = new HashMap<String, Integer>();
         list = (ListView)findViewById(R.id.list);
-
+        myAdpter = new MyAdpter(this);
+        list.setAdapter(myAdpter);
         db=new DB(online_pd.this,getFilesDir()+ File.separator+"data.db");
-
+        openuhf();
+        refreshData();
     }
 
+
+    private void refreshData()
+    {
+        Cursor cursor=db.getArchives();
+        if (cursor==null)
+        {
+            Toast.makeText(this,"加载档案数据失败,请退出重新进入",Toast.LENGTH_SHORT).show();
+            return ;
+        }
+        int i=0;
+        while (cursor.moveToNext())
+        {
+            Map<String,String> map = new HashMap<String, String>();
+            Log.i("RFIDLabelID",cursor.getString(1));
+            map.put("ArchivesNum",cursor.getString(0));
+            map.put("RFIDLabelID",cursor.getString(1));
+            map.put("TwoDCLabelID",cursor.getString(2));
+            map.put("FMaterial",cursor.getString(3));
+            map.put("ArchivesRoomNum",cursor.getString(4));
+            map.put("MiniatureNum",cursor.getString(5));
+            map.put("FileTitle",cursor.getString(6));
+            map.put("PreparationUnit",cursor.getString(7));
+            map.put("PreparationTime",cursor.getString(8));
+            map.put("DurationOfStorage",cursor.getString(9));
+            map.put("SecurityLevel",cursor.getString(10));
+            map.put("StoreroomID",cursor.getString(11));
+            map.put("SAreaID",cursor.getString(12));
+            map.put("CompactShelfID",cursor.getString(13));
+            map.put("ColNum",cursor.getString(14));
+            map.put("ABSide",cursor.getString(15));
+            map.put("GroupNum",cursor.getString(16));
+            map.put("ArchivesState",cursor.getString(18));
+            map.put("Remark",cursor.getString(19));
+            map.put("state","0");
+            mapList.add(map);
+            mapqueue.put(cursor.getString(1),i);
+            i++;
+        }
+        myAdpter.notifyDataSetChanged();
+    }
+
+
+
+    /**
+     * 打开UHF
+     */
     private void  openuhf()
     {
         reader = UhfReader.getInstance();
@@ -89,7 +152,7 @@ public class online_pd extends Activity {
         Util.initSoundPool(this);
         inventoryThread = new InventoryThread();
         inventoryThread.start();
-        scanmode=2;
+
 
 
     }
@@ -131,92 +194,68 @@ public class online_pd extends Activity {
         }
     };
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.createtask, menu);
-        return true;
-    }
-
-
-    private void openscan() {
-        try {
-            scanThread = new ScanThread(mHandler);
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(getApplicationContext(), "serialport init fail", Toast.LENGTH_SHORT)
-                    .show();
-            return;
-
-        }
-        scanThread.start();
-        //init sound
-        Util.initSoundPool(this);
-        scanmode=1;
-    }
-
-    /**
-     * 关闭扫描
-     */
-    private void closescan() {
-        if (scanThread != null) {
-            scanThread.interrupt();
-            scanThread.close();
-            scanThread = null;
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        closescan();
-        switch (id) {
-            case R.id.barcode:
-                if (scanmode==1)
-                    return true;
-                if (inventoryThread !=null)
-                {
-                    inventoryThread.CloseScan();
-                    inventoryThread.interrupt();
-                    inventoryThread=null;
-                }
-                UhfReader.getInstance().close();
-                if (scanThread !=null) {
-                    closescan();
-                    scanThread=null;
-                }
-                openscan();
-                Toast.makeText(online_pd.this,
-                        "条码", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.rfid:
-                if (scanmode==2)
-                    return true;
-                if (scanThread !=null) {
-                    closescan();
-                    scanThread=null;
-                }
-                UhfReader.getInstance().close();
-                if (inventoryThread !=null)
-                {
-                    inventoryThread.CloseScan();
-                    inventoryThread.interrupt();
-                    inventoryThread=null;
-                }
-                openuhf();
-                isstartrfid=false;
-                Toast.makeText(online_pd.this,
-                        "RFID", Toast.LENGTH_SHORT).show();
-                return true;
-
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.createtask, menu);
+//        return true;
+//    }
+//
+//
+//
+//
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        closescan();
+//        switch (id) {
+//            case R.id.barcode:
+//                if (scanmode==1)
+//                    return true;
+//                if (inventoryThread !=null)
+//                {
+//                    inventoryThread.CloseScan();
+//                    inventoryThread.interrupt();
+//                    inventoryThread=null;
+//                }
+//                UhfReader.getInstance().close();
+//                if (scanThread !=null) {
+//                    closescan();
+//                    scanThread=null;
+//                }
+//                openscan();
+//                Toast.makeText(online_pd.this,
+//                        "条码", Toast.LENGTH_SHORT).show();
+//                return true;
+//            case R.id.rfid:
+//                if (scanmode==2)
+//                    return true;
+//                if (scanThread !=null) {
+//                    closescan();
+//                    scanThread=null;
+//                }
+//                UhfReader.getInstance().close();
+//                if (inventoryThread !=null)
+//                {
+//                    inventoryThread.CloseScan();
+//                    inventoryThread.interrupt();
+//                    inventoryThread=null;
+//                }
+//                openuhf();
+//                isstartrfid=false;
+//                Toast.makeText(online_pd.this,
+//                        "RFID", Toast.LENGTH_SHORT).show();
+//                return true;
+//
+//        }
+//
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
 
     @Override
@@ -224,19 +263,13 @@ public class online_pd extends Activity {
 
 
         if (keyCode == 133 || keyCode == 134) {
-            if (scanmode==1)
-                scanThread.scan();
-            if (scanmode==2)
-            {
-                if (isstartrfid) {
-                    isstartrfid =false;
-                    inventoryThread.PauseScan();
-                }
-                else {
-                    isstartrfid=true;
-                    inventoryThread.StartScan();
-                }
-
+            if (isstartrfid) {
+                isstartrfid =false;
+                inventoryThread.PauseScan();
+            }
+            else {
+                isstartrfid=true;
+                inventoryThread.StartScan();
             }
         }
         if (keyCode==4)
@@ -250,6 +283,7 @@ public class online_pd extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        //关闭uhf
         if (inventoryThread !=null)
         {
             inventoryThread.CloseScan();
@@ -257,11 +291,74 @@ public class online_pd extends Activity {
             inventoryThread=null;
         }
 
-        if (scanThread!=null)
-        {
-            closescan();
-            scanThread=null;
 
+    }
+
+
+
+    private class MyAdpter extends BaseAdapter
+    {
+
+        TextView title,rfid,docid,state,pdstate;
+
+
+        LayoutInflater layoutInflater;
+        public MyAdpter(Context context)
+        {
+            layoutInflater=LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return mapList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return mapList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            view = layoutInflater.inflate(R.layout.list_pd,null);
+
+            title=(TextView)view.findViewById(R.id.title);
+            rfid=(TextView)view.findViewById(R.id.rfid);
+            docid=(TextView)view.findViewById(R.id.docid);
+            state=(TextView)view.findViewById(R.id.state);
+            pdstate=(TextView)view.findViewById(R.id.pdstate);
+
+            Map<String,String> map = mapList.get(i);
+            title.setText(map.get("FileTitle"));
+            rfid.setText("RIFD标签："+map.get("RFIDLabelID"));
+            docid.setText("档案号：" + map.get("docid"));
+            if (map.get("ArchivesState").equals("1"))
+                state.setText("状态：在架");
+            if (map.get("ArchivesState").equals("2"))
+                state.setText("状态：出库");
+            if (map.get("ArchivesState").equals("3"))
+                state.setText("状态：借阅");
+            if (map.get("ArchivesState").equals("4"))
+                state.setText("状态：注销");
+
+            if (map.get("state").equals("0"))
+            {
+                pdstate.setText("盘点状态：为扫描");
+                pdstate.setTextColor(getResources().getColor(R.color.red1));
+            }else
+            {
+                pdstate.setText("盘点状态：已扫描");
+                pdstate.setTextColor(getResources().getColor(R.color.green1));
+            }
+
+            return view;
         }
     }
+
 }
